aebf35af6077ad76a832a5b66258683118b89d57
Felipe Cagnin <cagnin.lima.2007@gmail.com>
2025-10-16 12:03:16 -0300

    Opa

--- projetoM/src/App.jsx
+++ projetoM/src/App.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
-import Home from './pages/home'  
+import Home from './pages/home'
+import Perguntas from './pages/perguntas'
 import './App.css'
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} /> 
+        <Route path="/" element={<Home />} />
+        <Route path="/perguntas-respostas" element={<Perguntas />} />
       </Routes>
     </BrowserRouter>
   )
