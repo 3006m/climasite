d7078a29daa80b7246acd5e1ed554ba6d38aa5aa
gamerjuniorjr07@gmail.com <gamerjuniorjr07@gmail.com>
2025-10-22 07:42:16 -0300

    texto do projeto

--- projetoM/src/pages/projeto.jsx
+++ projetoM/src/pages/projeto.jsx
@@ -1,11 +1,33 @@
-import React from 'react';
-import Navbar from '../components/Navbar';
+import React from "react";
+import Navbar from "../components/Navbar";
 
 export default function Projeto() {
-    return (
-        <main>
-             <Navbar />
-            <h1>Projeto</h1>
-        </main>
-    );
+  return (
+    <main>
+      <Navbar />
+      <h1>Projeto</h1>
+      <p>
+        O objetivo deste projeto é montar um sistema usando o ESP32, junto com
+        os sensores DHT11 e MQ2, para medir a umidade, a temperatura e a
+        presença de gases tóxicos no ambiente.
+      </p>
+      <p>
+        {" "}
+        A ideia é criar uma forma simples e prática de monitorar essas
+        condições, mostrando tudo por meio de LEDs. O DHT11 é o sensor
+        responsável por medir a temperatura e a umidade, enquanto o MQ2 detecta
+        gases tóxicos, ajudando a identificar se há algum risco no ambiente.
+        Para mostrar as informações, foram usados quatro LEDs: um indica a
+        umidade, outro mostra a temperatura, o terceiro sinaliza a presença de
+        gás tóxico e o último acende para indicar que o sistema está funcionando
+        corretamente.
+      </p>
+      <p>
+        Esse projeto mostra como é possivel usar diversas tecnologias juntas
+        para criar algo útil e eficiente para monitorar o ambiente de forma
+        simples. A parte de eletrônica ajudou a montar os fios e os sensores, a
+        de programação serviu pra fazer o ESP32 funcionar
+      </p>
+    </main>
+  );
 }
