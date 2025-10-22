14841a682969184f52505abdbcf4cfba40a28402
Felipe Cagnin <cagnin.lima.2007@gmail.com>
2025-10-22 13:37:59 -0300

    FOOOOOOOOOOOOOOOOOOOOOOOTERRRRRRRRRRRRRR2

--- projetoM/src/pages/software.jsx
+++ projetoM/src/pages/software.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Navbar from "../components/Navbar";
+import Footer from "../components/footer";
 import "../styles/css.css";
 
 export default function Software() {
